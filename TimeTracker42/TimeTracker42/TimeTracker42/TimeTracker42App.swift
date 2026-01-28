d479a0f8f6caa82c98eef661dbd44389c849ee58
Equinox201 <nsamarin@student.42singapore.sg>
2026-01-28 14:29:24 +0800

    added basic swiftui files

---
+++ TimeTracker42/TimeTracker42/TimeTracker42/TimeTracker42App.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTracker42App.swift
+//  TimeTracker42
+//
+//  Created by Nicolas Samarine on 28/1/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeTracker42App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
